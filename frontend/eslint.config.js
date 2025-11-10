6e61577d2413b0c99984242f6d8539a7594c266d
dimple-18 <kdimple1801@gmail.com>
2025-11-10 22:51:50 +0530

    added eslint and prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,58 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// eslint.config.js
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import pluginImport from 'eslint-plugin-import';
+import prettier from "eslint-config-prettier";
 
-export default defineConfig([
-  globalIgnores(['dist']),
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+   
+  js.configs.recommended,
+  prettier, 
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: { window: 'readonly', document: 'readonly' },
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      import: pluginImport,
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // React-specific rules
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+
+      // Hooks rules
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      // Refresh plugin
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      // Import organization
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            ['builtin', 'external'],
+            ['internal', 'parent', 'sibling', 'index'],
+          ],
+          'newlines-between': 'always',
+        },
+      ],
+
+      // Misc
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
+    settings: { react: { version: 'detect' } },
   },
-])
+];
